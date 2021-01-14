3b7f2cf60d55856213e02b8b84e2f51afd066d91
GorillaEnthusiast <77066059+GorillaEnthusiast@users.noreply.github.com>
2021-01-14 13:31:51 +1300

    Update Get Closest Player From Cursor.lua

--- Get Closest Player From Cursor.lua
+++ Get Closest Player From Cursor.lua
@@ -1,29 +1,22 @@
-
-local Camera = workspace.CurrentCamera
-local Players = game:GetService("Players")
-
-local LocalPlayer = Players.LocalPlayer
-local Mouse = LocalPlayer:GetMouse()
-
 function WTS(obj)
-	local objVector = Camera:WorldToScreenPoint(obj.Position)
-	return Vector2.new(objVector.X, objVector.Y)
+   local objVector = Camera:WorldToScreenPoint(obj.Position)
+   return Vector2.new(objVector.X, objVector.Y)
 end
 
 function getClosestPlayerFromMouse()
-	local ClosestPlayer = nil
-	local ClosestDistance = math.huge
- 
-	for i,  v in next, Players:GetPlayers() do
-	   if v ~= LocalPlayer and v.TeamColor ~= LocalPlayer.TeamColor then
-		  if v.Character and v.Character:FindFirstChild("Head") then
-			 local magnitude = (WTS(v.Character.Head) - Vector2.new(Mouse.X, Mouse.Y)).magnitude
-			 if magnitude < ClosestDistance then
-				ClosestPlayer = v
-				ClosestDistance = magnitude
-			end
-		 end
-	  end
+   local ClosestPlayer = nil
+   local ClosestDistance = math.huge
+
+   for i,  v in next, Players:GetPlayers() do
+      if v ~= LocalPlayer and v.TeamColor ~= LocalPlayer.TeamColor then
+         if v.Character and v.Character:FindFirstChild("Head") then
+            local magnitude = (WTS(v.Character.Head) - Vector2.new(Mouse.X, Mouse.Y)).magnitude
+            if magnitude < ClosestDistance then
+               ClosestPlayer = v
+               ClosestDistance = magnitude
+            end
+         end
+      end
    end
 
    return ClosestPlayer
