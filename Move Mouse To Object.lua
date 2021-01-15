c51a87ca7298e709e035e03bfa9aff771ffde7f2
GorillaEnthusiast <77066059+GorillaEnthusiast@users.noreply.github.com>
2021-01-15 21:44:52 +1300

    Create Move Mouse To Object.lua

---
+++ Move Mouse To Object.lua
@@ -0,0 +1,7 @@
+local Player = game:GetService("Players").LocalPlayer
+local Mouse = Player:GetMouse()
+
+function MoveMouseToObj(obj)
+   local ObjVector = Camera:WorldToScreenPoint(obj.Position)
+   mousemoverel(ObjVector.X - Mouse.X, ObjVector.Y - Mouse.Y)
+end
